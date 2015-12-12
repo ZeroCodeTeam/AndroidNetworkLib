f7fcf697670003db68e417367017d0c14e2cd61a
simikic <simikic.rade@gmail.com>
2015-12-13 00:12:53 +0100

    Add image upload support to network library.
    Change-Id: I0a27a9ba5432fab52d42843313aafc82bd3eaf79

---
+++ library/src/main/java/com/zerocodeteam/network/request/PhotoUploadRequest.java
@@ -0,0 +1,64 @@
+package com.zerocodeteam.network.request;
+
+import android.graphics.Bitmap;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Response;
+import com.android.volley.VolleyLog;
+
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Map;
+
+/**
+ * Created by Bane on 19.8.2015.
+ */
+public class PhotoUploadRequest<T> extends GsonRequest<T> {
+    private static final String FILE_PART_NAME = "file";
+
+    private MultipartEntityBuilder mBuilder = MultipartEntityBuilder.create();
+    private final Bitmap mImage;
+    private final String mImageName;
+
+    public PhotoUploadRequest(Object cookie, String url, Class<T> clazz, Map<String, String> headers, Bitmap image, String imageName, ResponseListener<T> listener, Response.ErrorListener errorListener) {
+        super(cookie, Method.POST, url, clazz, headers, null, listener, errorListener);
+        mImage = image;
+        mImageName = imageName;
+        buildMultipartEntity();
+    }
+
+    private void buildMultipartEntity() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        mImage.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        mBuilder.addBinaryBody(FILE_PART_NAME, byteArray, ContentType.create("image/jpeg"), mImageName);
+        mBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        mBuilder.setLaxMode().setBoundary("xx").setCharset(Charset.forName("UTF-8"));
+    }
+
+    @Override
+    public String getBodyContentType() {
+        String contentTypeHeader = mBuilder.build().getContentType().getValue();
+        return contentTypeHeader;
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            mBuilder.build().writeTo(bos);
+        } catch (IOException e) {
+            VolleyLog.e("IOException writing to ByteArrayOutputStream bos, building the multipart request.");
+        }
+        return bos.toByteArray();
+    }
+}
