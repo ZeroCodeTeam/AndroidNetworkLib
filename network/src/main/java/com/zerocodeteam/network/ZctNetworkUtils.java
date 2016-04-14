e6c7936f10f73733f443c14f8612f82a689de038
Rade Simikic <simikic.rade@gmail.com>
2016-04-15 00:25:38 +0200

    v0.1.5 Refactor code, introduce Bulder pattern.
    Add ZctNetworkUtils for non-ZctNetwork specific things.
    Add volley v1.0.
    Add comments to ZctNetwork lib.
    
    Change-Id: I1336313240276d62f68f22789c4931c696b5e8d5

---
+++ network/src/main/java/com/zerocodeteam/network/ZctNetworkUtils.java
@@ -0,0 +1,35 @@
+package com.zerocodeteam.network;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by rade on 14/04/16.
+ */
+public class ZctNetworkUtils {
+
+    private static Gson sGson;
+
+    public ZctNetworkUtils() {
+        sGson = new Gson();
+    }
+
+    /**
+     * Checks if there is network connectivity
+     *
+     * @return TRUE - connected, FALSE - not
+     */
+    public boolean isDeviceOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
+    }
+
+    public Gson getGsonInstance() {
+        return this.sGson;
+    }
+}
