94cbb5f8e5427c92ccc37208fbda46045a421892
Rade Simikic <simikic.rade@gmail.com>
2016-04-15 00:25:38 +0200

    v0.1.5 Refactor code, introduce Bulder pattern.
    Add ZctNetworkUtils for non-ZctNetwork specific things.
    Add volley v1.0.
    Add comments to ZctNetwork lib.
    
    Change-Id: I1336313240276d62f68f22789c4931c696b5e8d5

---
+++ sample/src/main/java/com/zerocodeteam/networktestapp/App.java
@@ -0,0 +1,28 @@
+package com.zerocodeteam.networktestapp;
+
+import android.app.Application;
+
+import com.zerocodeteam.network.ZctNetwork;
+
+/**
+ * Created by ZeroCodeTeam on 05/12/15.
+ */
+public class App extends Application {
+
+    private static final String DEFAULT_MSG = "Loading. Please wait...";
+    private static final Integer DEFAULT_DIALOG_TIME = 1000;
+
+    private static ZctNetwork sInstance;
+
+    public static ZctNetwork getNetworkInstance() {
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Init network module
+        sInstance = new ZctNetwork.Builder(this).defaultDialogMsg(DEFAULT_MSG).defaultLoggingEnabled(false/*BuildConfig.DEBUG*/).defaultMinDialogTime(DEFAULT_DIALOG_TIME).build();
+    }
+}
